9754ab3029ffc468c835ef5f8ddf2d7c36d1f290
gatana <sgatana@gmail.com>
2018-06-21 23:38:04 +0300

    [Chore] Add client

--- client/client.js
+++ client/client.js
@@ -3,8 +3,9 @@ const path = require('path')
 
 const PROTO_PATH = path.resolve(__dirname, '../protos/employee.proto')
 const proto = grpc.load(PROTO_PATH)
+const db = require('../databases')
 
 const client = new proto.employee.EmployeeService('localhost:50050', 
 grpc.credentials.createInsecure())
 
-client.getEmployee()
+client.getEmployee(db)
