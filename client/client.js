a25ce9e90d75d7ca35a89560a0a7355750224836
gatana <sgatana@gmail.com>
2018-06-21 23:13:40 +0300

    [Chore] Implement mali

--- client/client.js
+++ client/client.js
@@ -1,44 +1,10 @@
 const grpc = require('grpc')
 const path = require('path')
 
-const PROTO_PATH = path.resolve(__dirname, '../protos/leave.proto')
-
+const PROTO_PATH = path.resolve(__dirname, '../protos/employee.proto')
 const proto = grpc.load(PROTO_PATH)
 
-const client = new proto.leave.LeaveApplicationService('localhost:50050', 
+const client = new proto.employee.EmployeeService('localhost:50050', 
 grpc.credentials.createInsecure())
 
-const employee = {
-  valid: {
-    employeeId: 2345,
-    name: 'Stephen Gatana',
-    requestedDays: 6,
-    accruedDays: 8,
-  },
-  ineligible: {
-    employeeId: 2345,
-    name: 'Stephen Gatana',
-    requestedDays: 10,
-    accruedDays: 8,
-  },
-  invalid: {
-    employeeId: 2345,
-    name: 'Stephen Gatana',
-    requestedDays: -2,
-    accruedDays: 8,
-  }
-}
-
-client.applyForLeave(employee.ineligible, (error, res) => {
-  if(!error) {
-    if(res.eligible){
-      client.grantLeave(employee.valid, (error, response) => {
-        console.log('employee', response)
-      })
-    } else {
-      console.log('you do not qualify for a leave');
-    }
-  } else {
-    console.log('Error', error.message)
-  }
-});
+client.getEmployee()
---
+++ databases/index.js
@@ -0,0 +1,20 @@
+const employees = [
+  {
+    employeeId: 2345,
+    name: 'Stephen Gatana',
+    requestedDays: 6,
+    accruedDays: 8,
+  },
+  {
+    employeeId: 2134,
+    name: 'Steve Jobs',
+    requestedDays: 10,
+    accruedDays: 18,
+  },
+  {
+    employeeId: 1236,
+    name: 'John Doe',
+    requestedDays: 2,
+    accruedDays: 8,
+  }
+] 
