3253b7b99972545e973bab492176db574b16b8d6
gatana <sgatana@gmail.com>
2018-06-26 15:29:31 +0300

    - update client call to the server - add async.series

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 const grpc = require('grpc')
 const path = require('path')
+var async = require('async')
 
 const PROTO_PATH = path.resolve(__dirname, '../protos/employee.proto')
 const proto = grpc.load(PROTO_PATH)
@@ -8,4 +9,23 @@ const db = require('../databases')
 const client = new proto.employee.EmployeeService('localhost:50050', 
 grpc.credentials.createInsecure())
 
-client.getEmployee(db)
+function callEmployee(callback){
+  function featureCallback(err, res){
+    if(err){
+      return callback(err)
+    }
+    console.log(res)
+  }
+  client.getEmployee(1, featureCallback)
+}
+function main () {
+  async.series([
+    callEmployee,
+  ])
+}
+
+if (require.main === module) {
+  main()
+}
+
+exports.callEmployee = callEmployee
